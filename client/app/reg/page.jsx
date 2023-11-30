ecdb8d230867f28160112df2b35b0d21deec009b
Timofey Nagaev <nagaevmix33@gmail.com>
2023-11-30 21:15:17 +0700

    added themes? navbar

---
+++ client/app/reg/page.jsx
@@ -0,0 +1,12 @@
+
+import Container from '@mui/material/Container'
+import Paper from '@mui/material/Paper';
+import TextField from '@mui/material/TextField';
+
+export default function () {
+    return (
+        <Container maxWidth="lg">
+            <Paper elevation={10} sx={{ height: '20vh' }} />
+        </Container>
+    )
+}
---
+++ client/app/themes/lightTheme.js
@@ -0,0 +1,13 @@
+
+import { createTheme } from '@mui/material/styles';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+  background: {
+    default: '#ffffff',
+  },
+});
+
+export default darkTheme;
